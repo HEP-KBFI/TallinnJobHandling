de81779fd0de8c0a4131693b301057d9ca033ced
Laurits <laurits.tani@gmail.com>
2022-06-14 10:16:53 +0300

    nicer README, added necessary __init__.py file and minor changes in setup.sh -> to enforce python3 the law installation path was changed

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
+# Job handling
 
-cmsenv
+Make sure you are in a CMSSW area
+
+```bash
+cmsrel CMSSW_12_3_1
+cd $_ && cmsenv
+```
+
+Since this code depends on the ```HEP-KBFI/TallinnNtupleProducer```, make sure you have also this available locally
+
+```bash
+cd $CMSSW_BASE/src
+git clone git@github.com:HEP-KBFI/TallinnNtupleProducer.git
+scram b -j8
+```
+
+To set up the environment to use job handling:
+
+```bash
+cd $CMSSW_BASE/src
+git clone git@github.com:HEP-KBFI/TallinnJobHandling.git
+cd TallinnJobHandling
 source setup.sh
-(needs TallinnNtupleProducer)
+```
 
+## Running the jobhandling
+
+```bash
 law run ProdTallinnNTuples --version v1  --workflow slurm --CreateTallinnNtupleConfigs-workflow local --transfer-logs True --era 2017 --analysis hh-multilepton
+```
 
-Remove output: --remove-output N, N==DEPTH
+```Remove output: --remove-output N, N==DEPTH```
--- setup.sh
+++ setup.sh
@@ -1,32 +1,38 @@
 #!/usr/bin/env bash
 
+LAW_INSTALLATION_DIR="/home/laurits/share/law"
+
 action() {
     # determine the directory of this file
-    local this_file="$( [ ! -z "$ZSH_VERSION" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
-    local this_dir="$( cd "$( dirname "$this_file" )" && pwd )"
+    local SETUP_SCRIPT_PATH="$( [ ! -z "$ZSH_VERSION" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
+    local SETUP_SCRIPT_DIR="$( cd "$( dirname "$SETUP_SCRIPT_PATH" )" && pwd )"
+
+    # cd $LAW_INSTALLATION_DIR
+    # pip install -r requirements.txt --user
+    # cd $SETUP_SCRIPT_DIR
 
     # custom  (needed for our error handling)  law version in Torbens home:
-    export PATH="/home/tolange/share/law/bin/:$PATH"
-    export PYTHONPATH="/home/tolange/share/law/:$this_dir:$PYTHONPATH"
+    export PATH="$LAW_INSTALLATION_DIR/bin/:$PATH"
+    export PYTHONPATH="$LAW_INSTALLATION_DIR/:$SETUP_SCRIPT_DIR:$PYTHONPATH"
 
-    export LAW_HOME="$this_dir/.law"
-    export LAW_CONFIG_FILE="$this_dir/law.cfg"
+    export LAW_HOME="$SETUP_SCRIPT_DIR/.law"
+    export LAW_CONFIG_FILE="$SETUP_SCRIPT_DIR/law.cfg"
 
     law index --verbose
 
     # where should log files go (if not debug and sucessfull these are immidieatly cleaned -> home should be ok)
-    export ANALYSIS_LOGFILE_PATH="$this_dir/logs"
+    export ANALYSIS_LOGFILE_PATH="$SETUP_SCRIPT_DIR/logs"
     mkdir -p $ANALYSIS_LOGFILE_PATH
 
     #workarea where can we safely create files e.g. not hdfs but scratch?
-    export ANALYSIS_WORKAREA="$this_dir"
+    export ANALYSIS_WORKAREA="$SETUP_SCRIPT_DIR"
 
     # config like output e.g. scratch-persistent
     export ANALYSIS_CONFIG_PATH="$ANALYSIS_PATH/config"
     # root like file output e.g hdfs?
     export ANALYSIS_ROOT_PATH="$ANALYSIS_PATH/root"
 
-    export ANALYSIS_PATH="$this_dir"
+    export ANALYSIS_PATH="$SETUP_SCRIPT_DIR"
     export ANALYSIS_DATA_PATH="$ANALYSIS_PATH/data"
 
     source "$( law completion )" ""
